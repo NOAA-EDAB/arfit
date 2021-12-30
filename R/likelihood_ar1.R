d9f3f4206bcca072fde55e611aa22718353d14ac
andybeet <22455149+andybeet@users.noreply.github.com>
2021-12-30 12:24:51 -0500

    reworked likelihood function to be more general. grid and opt routines use the same function.

--- R/likelihood_ar1.R
+++ R/likelihood_ar1.R
@@ -2,7 +2,6 @@
 #'
 #'
 #'
-#'@param beta Numeric vector. (px1). Defining alpha and beta.
 #'@param rho Numeric scalar. Autoregressive parameter
 #'@param dataf Data frame. (nx2). Defining xt and yt values
 #'@param hypothesis Character string. "null" or "alt" determines whether to evaluate
@@ -14,41 +13,40 @@
 #'
 #' @export
 
-likelihood_ar1 <- function(beta,rho,dataf,hypothesis="null") {
+likelihood_ar1 <- function(rho,dataf,hypothesis="null") {
 
+  nT <- nrow(dataf)
   hypothesis <- tolower(hypothesis)
 
   x <- dataf$x
   y <- dataf$y
-  if (hypothesis == "null"){
+  if (tolower(hypothesis) == "null"){
     x1 <- x[1]
+    # Design matrix (2:nT)
+    xt <- cbind(rep(1,nT-1))
+    xtm1 <- xt
   } else {
     x1 <- cbind(1,x[1])
+    # Design matrix (2:nT)
+    xt <- cbind(rep(1,nT-1),as.vector(x[2:nT]))
+    # Design matrix (1:nT-1)
+    xtm1 <- cbind(rep(1,nT-1),as.vector(x[1:(nT-1)]))
   }
   y1 <- y[1]
-  nT <- length(x)
 
   # response
   yt <- as.vector(y[2:nT] )
   ytm1 <- as.vector(y[1:(nT-1)])
 
-  if (hypothesis == "null") {
-    part1 <- .5*log(1-rho^2)
-    part2a <- (1-rho^2)*((y1-beta)^2)
-    part2b <- sum((yt- rep(beta,nT-1) - rho*ytm1 + rho*rep(beta,nT-1))^2)
-    like <- part1 - (nT/2)*log(part2a + part2b)
-  } else {
-    # Design matrix (2:nT)
-    xt <- cbind(rep(1,nT-1),as.vector(x[2:nT]))
-    # Design matrix (1:nT-1)
-    xtm1 <- cbind(rep(1,nT-1),as.vector(x[1:(nT-1)]))
-    # Likelihood evaluation
-    part1 <- .5*log(1-rho^2)
-    part2a <- (1-rho^2)*((y1-x1%*%beta)^2)
-    part2b <- sum((yt- xt%*%beta - rho*ytm1 + rho*xtm1%*%beta)^2)
-    like <- part1 - (nT/2)*log(part2a + part2b)
-  }
+  # MLE for beta conditional on rho
+  beta <- est_beta_given_rho(rbind(x1,xt),c(y1,yt),rho,hypothesis)
 
-  return(like)
+  #likelihood
+  part1 <- .5*log(1-rho^2)
+  part2a <- (1-rho^2)*((y1-x1%*%beta)^2)
+  part2b <- sum((yt- xt%*%beta - rho*ytm1 + rho*xtm1%*%beta)^2)
+  like <- part1 - (nT/2)*log(part2a + part2b)
+
+  return(-like)
 
 }
