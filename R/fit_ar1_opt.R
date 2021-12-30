bb1b5652d90b16e5a8d74cd2cf5cf6ffd58632b7
andybeet <22455149+andybeet@users.noreply.github.com>
2021-12-30 12:23:41 -0500

    added fitting routine that uses optim instead of grid search - fit_ar1_opt.r

---
+++ R/fit_ar1_opt.R
@@ -0,0 +1,41 @@
+#' Fit linear model with trend using optimization routine
+#'
+#'
+#'
+#'
+#' @export
+
+fit_ar1_opt <- function(data,rho,hypothesis) {
+
+  nT <- nrow(data)
+
+  if(tolower(hypothesis) == "null") {
+    xt <- cbind(rep(1,nT))
+  } else {
+    xt <- cbind(rep(1,nT),data$x)
+  }
+  yt <- data$y
+
+
+  outList <- optim(par=rho,fn=likelihood_ar1,gr=NULL,data,hypothesis,method="L-BFGS-B",lower=-.99,upper=.99)
+
+  maxRho <- outList$par
+  maxLike <- -outList$value
+
+  # MLE for beta conditional on rho
+  maxBeta <- est_beta_given_rho(xt,yt,maxRho,hypothesis)
+
+  # calculate MLE for sigma
+  ut <- yt- xt%*%maxBeta
+  maxSigma <- sqrt((((1-maxRho^2)*ut[1]^2) + sum((ut[2:nT]-maxRho*ut[1:(nT-1)])^2))/nT)
+
+  # calculate the variance of Beta
+  xt1 <- xt[2:nT]
+  xtm1 <- xt[1:(nT-1)]
+  varBeta <- 1/((1/maxSigma^2)*((1-maxRho^2)*xt[1]^2 + sum((xt1-maxRho*xtm1)^2)))
+
+
+
+  return(list(betaEst=maxBeta,rhoEst=maxRho,sigmaEst=maxSigma,varBeta=varBeta,likelihood=maxLike))
+
+}
