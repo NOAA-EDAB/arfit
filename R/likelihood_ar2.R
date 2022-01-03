20b1d9c099760d58a28baf08b9b190871fc4bafb
andybeet <22455149+andybeet@users.noreply.github.com>
2022-01-03 15:00:29 -0500

    added beta estimation|rhos in likelihood rather than passing beta as an argument

--- R/likelihood_ar2.R
+++ R/likelihood_ar2.R
@@ -1,40 +1,55 @@
 #' Evaluates the likelihood of AR2
 #'
 #' Uses full likelihood as explained in Beach and MacKinnon
-#' The first two data points are used in the likelihood 
+#' The first two data points are used in the likelihood
 #'
-#'@param data
-#'@param beta Numeric vector. intercept and slope term
-#'@param rho1
-#'@param rho2
+#'@param data dd
+#'@param rho1 1st order Autoregressive parameter
+#'@param rho2 2nd order Autoregressive parameter
+#'@param hypothesis Character string. "null" or "alt" determines whether to evaluate
+#'under the null or alternative hypothesis. "null" fixes linear component, beta = 0.
 #'
+#'@return -ve Log likelihood
 #'
+#'@export
 
-likelihood_ar2 <- function(data,beta,rho1,rho2) {
-  
-  T <- nrow(data)
-  xt <- data$x
+likelihood_ar2 <- function(data,rho1,rho2,hypothesis="null") {
+
+  hypothesis <- tolower(hypothesis)
+  nT <- nrow(data)
+  x <- data$x
   yt <- data$y
-  
+
+  if (tolower(hypothesis) == "null"){
+    # Design matrix (2:nT)
+    xt <- cbind(rep(1,nT))
+  } else {
+    # Design matrix (2:nT)
+    xt <- cbind(rep(1,nT),as.vector(x[1:nT]))
+  }
+
+  # MLE for beta conditional on rhos
+  beta <- est_beta_given_rho(xt,yt,rho1,rho2,hypothesis)
+
   # first two residuals
   A1 <- yt[1] - beta[1] - xt[1]*beta[2]
   A2 <- yt[2] - beta[1] - xt[2]*beta[2]
   # all residuals
   At <- yt - beta[1] - xt*beta[2]
-  res <- At[3:T]- rho1*At[2:(T-1)] - rho2*At[1:(T-2)]
-  
+  res <- At[3:nT]- rho1*At[2:(nT-1)] - rho2*At[1:(nT-2)]
+
   # Evaluate the Sum Squares of the Residuals
   SSR1 <- (1-rho1^2)*A1^2
   SSR2 <- -2*rho1*(1+rho2)*A1*A2
   SSR3 <- (1-rho2^2)*A2^2
   SSR4 <- sum(res^2)
-  
-  part1 <- -(T/2)*log(SSR1+SSR2+SSR3+SSR4)
+
+  part1 <- -(nT/2)*log(SSR1+SSR2+SSR3+SSR4)
   part2 <- log(1+rho2) + 0.5*log(1-rho1-rho2) + 0.5*log(1+rho1-rho2)
-  
+
   # likelihood
   like <- part1 + part2
-  
-  return(like)
-  
+
+  return(-like)
+
 }
