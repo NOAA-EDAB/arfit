dc52b8b445dc79efd3b91231799af8496cca881f
andybeet <22455149+andybeet@users.noreply.github.com>
2022-01-06 16:37:25 -0500

    Added package documentation

---
+++ R/arfit.R
@@ -0,0 +1,22 @@
+#'arfit: Test for a trend in presence of autocorrelation
+#'
+#'Tools to test for a trend in presence of autocorrelation. All tools to
+#'reproduce results in publication
+#'
+#'\itemize{
+#'\item Simulates data
+#'\item Evaluates likelihood
+#'\item Fits likelihood
+#'\item Estimates MLE for model
+#'\item Example provides
+#'\item Tools for su=imulation study
+#'}
+#'
+#'@section References:
+#'
+#'To learn more about using \code{arfit}, start with the vignette: \code{browseVignettes(package="arfit")} or click the index below
+#'
+#'
+#' @docType package
+#' @name arfit
+NULL
