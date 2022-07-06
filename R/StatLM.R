0eb8db402543f5ff7cb4962e46498f2753ba6b8a
kimberly-bastille <kimberly.bastille@gmail.com>
2022-07-06 11:23:38 -0400

    add geom_lm() and add plots to rmd

---
+++ R/StatLM.R
@@ -0,0 +1,80 @@
+## StatLM Stat protto based on ecodata StatGLS
+StatLM <- ggplot2::ggproto("StatLM",
+                            ggplot2::Stat,
+                            required_aes = c("x", "y"),
+                            compute_group = function(data, scales, warn) {
+                              
+                              `%>%` <- magrittr::`%>%`
+                              data <- data %>%
+                                dplyr::arrange(x) %>%
+                                #Fill in time steps if there are missing values
+                                tidyr::complete(x = tidyr::full_seq(min(data$x):max(data$x),1)) %>% 
+                                # Select last 11 years
+                                dplyr::filter(x %in% (max(x)-11):max(x))
+                            
+                              
+                              #Model fitting -------------------------------------------------------
+                              constant_ar1 <-
+                                try(nlme::gls(y ~ 1,
+                                              data = data,
+                                              correlation = nlme::corAR1(form = ~x),
+                                              na.action = na.omit))
+                              if (class(constant_ar1) == "try-error"){
+                                return(best_lm <- data.frame(model = NA,
+                                                             aicc  = NA,
+                                                             coefs..Intercept = NA,
+                                                             coefs.time = NA,
+                                                             coefs.time2 = NA,
+                                                             pval = NA))
+                              }
+                              
+                              
+                              # Linear model with AR1 error
+                              linear_ar1 <-
+                                try(nlme::gls(y ~ x,
+                                              data = data,
+                                              correlation = nlme::corAR1(form = ~x),
+                                              na.action = na.omit))
+                              if (class(linear_ar1) == "try-error"){
+                                return(best_lm <- data.frame(model = NA,
+                                                             aicc  = NA,
+                                                             coefs..Intercept = NA,
+                                                             coefs.time = NA,
+                                                             coefs.time2 = NA,
+                                                             pval = NA))
+                                
+                              }
+                              
+                              
+                              # Calculate AICs for all models
+                              df_aicc <-
+                                data.frame(model = c("linear_ar1"),
+                                           aicc  = c(AICcmodavg::AICc(linear_ar1)),
+                                           coefs = rbind(c(coef(linear_ar1),  NA)),
+                                           # Calculate overall signifiance (need to use
+                                           # ML not REML for this)
+                                           pval = c(anova(update(constant_ar1, method = "ML"),
+                                                          update(linear_ar1, method = "ML"))$`p-value`[2]))
+                              
+                              best_lm <- df_aicc %>%
+                                dplyr::filter(aicc == min(aicc)) #Select model with lowest AICc
+                              
+                              
+                              model <- linear_ar1
+                              
+                              
+                              #if (best_lm$pval < 0.05){ Temporarily test with no if
+                                
+                                newtime <- seq(min(data$x), max(data$x), length.out=length(data$x))
+                                newdata <- data.frame(x = newtime,
+                                                      x2 = newtime^2)
+                                lm_pred <- AICcmodavg::predictSE(model,
+                                                                 newdata = newdata,
+                                                                 se.fit = TRUE) #Get BLUE
+                                data <- data.frame(x = data$x,
+                                                   y = lm_pred$fit)
+                                
+                                return(data)
+                              #}
+                            }
+)
