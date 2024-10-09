27220601d1e9f1e3f598a2695c22cc41cb624168
andybeet <22455149+andybeet@users.noreply.github.com>
2024-10-09 11:03:17 -0400

    amends data if blocks of NA are in first or last spot

--- R/check_data_validation.R
+++ R/check_data_validation.R
@@ -2,6 +2,7 @@
 #'
 #' Check column names, missing data, consecutive equally spaced time units
 #'
+#'
 
 check_data_validation <- function(dataSet) {
 
@@ -14,22 +15,42 @@ check_data_validation <- function(dataSet) {
   }
 
   # check for equally spaced time series
-  spacing <- diff(dataSet$x)
+    spacing <- diff(dataSet$x)
   if(!all(spacing == spacing[1])){
     stop("Time (x field) is not equally spaced. You can not apply this test")
   } else {
     # maybe make x = 1:n?
   }
 
-  # check for missing data
-  if(any(is.na(dataSet$y))){
-    # can not proceed if NA is first or last position
-    if(head(is.na(dataSet$y),1) | tail(is.na(dataSet$y),1)) {
-      stop("Missing data in your response variable at the begining or end of the time series is not currently permitted")
-    } else {
-      missingValues <- which(is.na(dataSet$y))
-      warning("You have some missing data in your response variable (y).")
+
+  # recursive incase blocks of NA at start or end of data
+  clean <- F
+  while(!clean) {
+    clean <- T
+    # check for missing data
+    if(head(is.na(dataSet$y),1) ) {
+      # NA in first position. Drop it.
+      # Treat the timeseries as length n-1
+      dataSet <- dataSet |>
+        dplyr::slice_tail(n = -1)
+      clean <- F
     }
+
+    if (tail(is.na(dataSet$y),1)) {
+      # NA in last position. Drop it
+      # Treat the timeseries as length n-1
+      dataSet <- dataSet |>
+        dplyr::slice_head(n = -1)
+      clean <- F
+    }
+
+  }
+
+
+  # check for other missing data
+  if(any(is.na(dataSet$y))){
+    missingValues <- which(is.na(dataSet$y))
+    warning("You have some missing data in your response variable (y)")
   } else {
     missingValues <- NULL
   }
